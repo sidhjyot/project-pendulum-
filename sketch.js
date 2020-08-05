6da153f6229f994130dca99ea7a0efc5394c0c13
sidhjyot <65850342+sidhjyot@users.noreply.github.com>
2020-08-05 14:51:48 +0530

    Fixed some bugs
    1. Created a bobDiameter variable
    2. Avoid using fixed values, like line no. 27  --> width and height considers the canvas() dimensions.
    3. Changed the sequence of displaying objects

--- sketch.js
+++ sketch.js
@@ -17,27 +17,37 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	bob1 = new Bob(400,500,1);
-	bob2 = new Bob(350,500,5);
-	bob3 = new Bob(300,500,5);
-	bob4 = new Bob(450,500,5);
-	bob5 = new Bob(500,500,5);
 
-	roof1 = new Roof (400,100,600,30);
+	roof1 = new Roof (width/2,height/4,width/7,20);
 
-                           
+	bobDiameter=40;
+
+
+	startBobPositionX=width/2;
+	startBobPositionY=height/4+500;
+
+	bob1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	bob2=new Bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+
+	// bob3 = new Bob(300,500,5);
+	// bob4 = new Bob(450,500,5);
+	// bob5 = new Bob(500,500,5);
+
+	
+
+                        
 
 	string1= new Strings(bob1.body,roof1.body,-bobDiameter*2,0);
 	string2= new Strings(bob2.body,roof1.body,-bobDiameter*2,0);
-	string3= new Strings(bob3.body,roof1.body,-bobDiameter*2,0);
-	string4= new Strings(bob4.body,roof1.body,-bobDiameter*2,0);
-	string5= new Strings(bob5.body,roof1.body,-bobDiameter*2,0);
+	// string3= new Strings(bob3.body,roof1.body,-bobDiameter*2,0);
+	// string4= new Strings(bob4.body,roof1.body,-bobDiameter*2,0);
+	// string5= new Strings(bob5.body,roof1.body,-bobDiameter*2,0);
    
 	
 	Engine.run(engine);
@@ -48,19 +58,20 @@ function setup() {
 function draw() {    
   rectMode(CENTER);
   background(255);
-  string1.display();
-  /*string2.display();
-  string3.display();
+  
+  /*string3.display();
   string4.display();
   string5.display();*/
-  
+  roof1.display();
+
   bob1.display();
-  bob5.display();
-  bob4.display();
-  bob3.display(); 
+//   bob5.display();
+//   bob4.display();
+//   bob3.display(); 
   bob2.display();
 
-  roof1.display();
+  string1.display();
+  string2.display();
 
   drawSprites();
  
@@ -69,7 +80,7 @@ function draw() {
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(bob2.body,bob2.body.position,{x:85,y:-85});
+		//Matter.Body.applyForce(bob2.body,bob2.body.position,{x:85,y:-85});
 		
 	}
 	
